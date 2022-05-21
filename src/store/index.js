0a205527a9467b6fd1be47fdb5cea5f026f14540
Il Kwon <alexryu1105@naver.com>
2022-05-22 05:16:26 +0900

    store/index.js 'dealStore' 추가

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,12 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 import boardStore from "./modules/boardStore.js";
+import dealStore from "./modules/dealStore.js";
 
 const store = new Vuex.Store({
   modules: {
     boardStore,
+    dealStore,
   },
 });
 
