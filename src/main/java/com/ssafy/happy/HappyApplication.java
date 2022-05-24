f64b89966a2ab19eba5142757b6c8d0a09715603
Il Kwon <alexryu1105@naver.com>
2022-05-24 10:57:06 +0900

    프로젝트 구조 리팩토링 후 MapperScan 수정

--- src/main/java/com/ssafy/happy/HappyApplication.java
+++ src/main/java/com/ssafy/happy/HappyApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@MapperScan("com.ssafy.happy.model.repo")
+@MapperScan("com.ssafy.happy.*.model.repo")
 public class HappyApplication {
 
 	public static void main(String[] args) {
