ec5a16eef1b747c06a6df987be3365c89ee385f7
choijoohee213 <choijoohee@naver.com>
2022-05-25 01:49:51 +0900

    최신뉴스 로직 추가(네이버 검색 api로 크롤링 해오도록 구현)

--- src/main/java/com/ssafy/happy/HappyApplication.java
+++ src/main/java/com/ssafy/happy/HappyApplication.java
@@ -3,9 +3,11 @@ package com.ssafy.happy;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @MapperScan("com.ssafy.happy.**.model.repo")
+@PropertySource("classpath:/application.properties")
 public class HappyApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/ssafy/happy/news/controller/NewsController.java
@@ -0,0 +1,29 @@
+package com.ssafy.happy.news.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.happy.news.domain.NaverCrawler;
+
+@RestController
+@RequestMapping("/news")
+public class NewsController {
+
+	private final NaverCrawler crawler;
+
+	public NewsController(NaverCrawler crawler) {
+		this.crawler = crawler;
+	}
+
+	@GetMapping("/")
+	public ResponseEntity<List<Map<String, Object>>> getNews() {
+		return new ResponseEntity<>(crawler.getNews(), HttpStatus.OK);
+	}
+}
