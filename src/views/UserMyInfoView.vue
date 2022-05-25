3795f7f19f3bd1152f3d15bff6fea5946148d2cd
Il Kwon <alexryu1105@naver.com>
2022-05-26 00:31:19 +0900

    마이페이지의 관심지역 관리 삭제

--- src/views/UserMyInfoView.vue
+++ src/views/UserMyInfoView.vue
@@ -7,7 +7,6 @@
           <p><router-link :to="{ name: 'userMyInfoHome' }" class="link">마이페이지</router-link></p>
           <p><router-link :to="{ name: 'userModify' }" class="link">회원정보 수정</router-link></p>
           <p><router-link :to="{ name: 'userWithdraw' }" class="link">회원탈퇴</router-link></p>
-          <p><router-link :to="{ name: 'house' }" class="link">관심지역 관리</router-link></p>
         </div></b-col
       >
       <b-col cols="9" class="h-100 text-left">
