91c0dedeca5437d3b8a3cb96b148cdfe71442dbe
choijoohee213 <choijoohee@naver.com>
2022-05-26 02:06:56 +0900

    라이브러리 삭제

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import VeeValidate from "vee-validate";
-import VueKakaoSdk from "vue-kakao-sdk";
 
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
@@ -17,9 +16,6 @@ Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
 Vue.use(chartsVue);
-
-const apiKey = decodeURIComponent(process.env.VUE_APP_KAKAO_LOGIN_API_KEY);
-Vue.use(VueKakaoSdk, { apiKey });
 
 Vue.config.productionTip = false;
 
