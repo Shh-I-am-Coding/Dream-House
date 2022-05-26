53bdf34095c6b475ee44bef3303d2c74e54ad44b
choijoohee213 <choijoohee@naver.com>
2022-05-26 18:29:18 +0900

    관심 아파트 추가/해제 버튼을 이미지 버튼으로 변경

--- src/components/deal/DealList.vue
+++ src/components/deal/DealList.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <div style="position: absolute; top: 210px; left: 10px; z-index: 3">
-      <b-button v-b-toggle.sidebar-aptList class="mr-2 yBtn">아파트 리스트</b-button>
-      <b-button v-b-toggle.sidebar-interestList class="interBtn" @click="isLogin">관심지역</b-button>
+      <b-button v-b-toggle.sidebar-aptList class="mr-2 yBtn"><b>아파트 리스트</b></b-button>
+      <b-button v-b-toggle.sidebar-interestList class="interBtn" @click="isLogin"><b>관심 아파트</b></b-button>
     </div>
+
     <b-sidebar id="sidebar-aptList" title="아파트 리스트" shadow style="width: 20vw">
       <b-container v-if="deals && deals.length != 0" class="bv-example-row mt-3 overflow-auto">
         <deal-list-item v-for="(deal, index) in deals" :key="index" :deal="deal" :interested="0" v-b-toggle="['sidebar-dealInfo']" />
@@ -20,7 +21,9 @@
       </b-container>
     </b-sidebar>
     <b-sidebar id="sidebar-dealInfo" title="아파트 정보" right shadow width="20vw">
-      <b-container v-if="dealInfos && dealInfos.length != 0" class="bv-example-row">
+      <b-container v-if="dealInfos && dealInfos.length != 0" class="bv-example-row" style="position: relative; top: -10vh">
+        <input class="favoriteBtn bounce-top" type="image" :src="require('@/assets/img/untouch.png')" v-if="!isInInter" @click="insertInterest" name="submit" width="15%" height="5%" alt="좋아요" />
+        <input class="favoriteBtn bounce-top" type="image" :src="require('@/assets/img/touch.png')" v-else @click="delInterest" name="submit" width="15%" height="5%" alt="좋아요 해제" />
         <img :src="getImgName()" style="width: 350px; height: 300px" />
         <b-row>
           <b-col id="infoName">{{ dealInfos[0].aptName }} </b-col>
@@ -32,6 +35,7 @@
         <b-row v-if="dealInfos[0].jibun">
           <b-col class="address">지번 주소 : {{ dealInfos[0].dongName }} {{ dealInfos[0].jibun }}</b-col>
         </b-row>
+
         <div style="border-top: 1px solid; margin-top: 5px; padding-top: 5px">거래 정보</div>
         <b-container class="bv-example-row mt-3 overflow-auto dealInfoContainer">
           <div v-for="(dealInfo, index) in dealInfos" :key="index">
@@ -41,12 +45,8 @@
         <deal-avg-chart v-if="userInfo" />
         <div v-else style="font-size: 1.1rem">회원 가입하고 더 많은 정보를 얻어보세요~😉</div>
       </b-container>
-      <b-row style="margin-top: 20px">
-        <b-col cols="6"><b-button class="interBtn conBtn" v-b-toggle="['sidebar-dealInfo']">돌아가기</b-button></b-col>
-        <b-col cols="6" v-show="userInfo" style="margin-bottom: 20px">
-          <b-button v-if="!isInInter" class="yBtn conBtn" @click="insertInterest">관심 등록</b-button>
-          <b-button v-if="isInInter" class="yBtn conBtn" @click="delInterest">관심 해제</b-button>
-        </b-col>
+      <b-row style="position: relative; top: -5vh">
+        <b-col><b-button class="interBtn conBtn" v-b-toggle="['sidebar-dealInfo']">돌아가기</b-button></b-col>
       </b-row>
     </b-sidebar>
   </div>
@@ -114,8 +114,8 @@ export default {
       if (this.userInfo == null) {
         console.log("NeedLogin");
         Swal.fire({
-          title: "로그인이 필요합니다! 💦",
-          text: "회원가입을 하고 관심지역을 등록해 보세요.",
+          title: "로그인 필요! 💦",
+          text: "회원가입을 하고 관심 아파트를 등록해 보세요.",
           icon: "warning",
           confirmButtonText: "확인",
         });
@@ -124,8 +124,8 @@ export default {
     insertInterest() {
       if (this.userInfo.id == null) {
         Swal.fire({
-          title: "로그인이 필요합니다! 💦",
-          text: "회원가입을 하고 관심지역을 등록해 보세요.",
+          title: "로그인 필요! 💦",
+          text: "회원가입을 하고 관심 아파트를 등록해 보세요.",
           icon: "warning",
           confirmButtonText: "확인",
         });
@@ -137,8 +137,8 @@ export default {
         console.log("create", params);
         this.createInterest(params).then(
           Swal.fire({
-            title: "관심지역이 등록되었습니다! ",
-            text: "관심지역 목록에서 확인해보세요!.",
+            title: "관심 아파트에 등록! 🏠 ",
+            text: "관심 아파트 목록에서 확인해보세요!",
             icon: "success",
             confirmButtonText: "확인",
           })
@@ -147,7 +147,8 @@ export default {
     },
     delInterest() {
       Swal.fire({
-        title: "정말 삭제하시겠습니까?",
+        title: "관심 목록에서 삭제하시겠습니까? 🤔",
+        icon: "warning",
         showDenyButton: true,
         confirmButtonText: "삭제",
         denyButtonText: `취소`,
@@ -158,7 +159,7 @@ export default {
             id: this.userInfo.id,
             aptCode: this.deal.aptCode,
           };
-          this.deleteInterest(params).then(Swal.fire("삭제완료!", "", "success"));
+          this.deleteInterest(params).then(Swal.fire("삭제 완료!", "", "success"));
         }
       });
     },
@@ -225,61 +226,39 @@ export default {
   max-height: 300px;
   margin-bottom: 1rem;
 }
-.yBtn {
-  color: black;
-  background-color: #ffd071;
-}
-.yBtn:hover {
-  color: black;
-  background-color: #ffc756;
-}
-.yBtn:not(:disabled):not(.disabled):active {
-  color: black;
-  background-color: #ffd071;
-}
-.yBtn:not(:disabled):not(.disabled).active {
-  color: black;
-  background-color: #ffd071;
-}
-.yBtn:focus {
-  color: black;
-  background-color: #ffd071;
-}
-.yBtn.focus {
-  color: black;
-  background-color: #ffd071;
-}
+.yBtn,
+.yBtn:hover,
+.yBtn:not(:disabled):not(.disabled):active,
+.yBtn:not(:disabled):not(.disabled).active,
+.yBtn:focus,
+.yBtn.focus,
 .show > .yBtn.dropdown-toggle {
   color: black;
   background-color: #ffd071;
 }
-.interBtn {
-  background-color: #fcb666;
-  color: black;
-}
+
 .interBtn:hover {
   color: black;
   background-color: #fda846;
 }
-.interBtn:not(:disabled):not(.disabled):active {
-  color: black;
-  background-color: #fcb666;
-}
-.interBtn:not(:disabled):not(.disabled).active {
-  color: black;
-  background-color: #fcb666;
-}
-.interBtn:focus {
-  color: black;
-  background-color: #fcb666;
-}
-.interBtn.focus {
-  color: black;
-  background-color: #fcb666;
-}
+.interBtn,
+.interBtn:not(:disabled):not(.disabled):active,
+.interBtn:not(:disabled):not(.disabled).active,
+.interBtn:focus,
+.interBtn.focus,
 .show > .interBtn.dropdown-toggle {
   color: black;
   background-color: #fcb666;
+}
+.favoriteBtn {
+  position: relative;
+  top: 48vh;
+  float: right;
+  width: 15%;
+  z-index: 7;
+}
+.favoriteBtn:hover {
+  cursor: pointer;
 }
 .conBtn {
   width: 12vh;
@@ -288,5 +267,117 @@ export default {
 .card-title {
   font-size: 1.2em;
   font-weight: bold;
+}
+
+.bounce-top {
+  -webkit-animation: bounce-top 1.2s ease-in-out 1.6s 3 both;
+  animation: bounce-top 1.2s ease-in-out 1.6s 3 both;
+  background-color: #f8f9fa !important;
+  border: none;
+}
+
+@-webkit-keyframes bounce-top {
+  0% {
+    -webkit-transform: translateY(-20px);
+    transform: translateY(-20px);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+    opacity: 1;
+  }
+  24% {
+    opacity: 1;
+  }
+  40% {
+    -webkit-transform: translateY(-18px);
+    transform: translateY(-18px);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+  }
+  65% {
+    -webkit-transform: translateY(-12px);
+    transform: translateY(-12px);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+  }
+  82% {
+    -webkit-transform: translateY(-6px);
+    transform: translateY(-6px);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+  }
+  93% {
+    -webkit-transform: translateY(-4px);
+    transform: translateY(-4px);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+  }
+  25%,
+  55%,
+  75%,
+  87% {
+    -webkit-transform: translateY(0px);
+    transform: translateY(0px);
+    -webkit-animation-timing-function: ease-out;
+    animation-timing-function: ease-out;
+  }
+  100% {
+    -webkit-transform: translateY(0px);
+    transform: translateY(0px);
+    -webkit-animation-timing-function: ease-out;
+    animation-timing-function: ease-out;
+    opacity: 1;
+  }
+}
+@keyframes bounce-top {
+  0% {
+    -webkit-transform: translateY(-20px);
+    transform: translateY(-20px);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+    opacity: 1;
+  }
+  24% {
+    opacity: 1;
+  }
+  40% {
+    -webkit-transform: translateY(-18px);
+    transform: translateY(-18px);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+  }
+  65% {
+    -webkit-transform: translateY(-12px);
+    transform: translateY(-12px);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+  }
+  82% {
+    -webkit-transform: translateY(-6px);
+    transform: translateY(-6px);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+  }
+  93% {
+    -webkit-transform: translateY(-4px);
+    transform: translateY(-4px);
+    -webkit-animation-timing-function: ease-in;
+    animation-timing-function: ease-in;
+  }
+  25%,
+  55%,
+  75%,
+  87% {
+    -webkit-transform: translateY(0px);
+    transform: translateY(0px);
+    -webkit-animation-timing-function: ease-out;
+    animation-timing-function: ease-out;
+  }
+  100% {
+    -webkit-transform: translateY(0px);
+    transform: translateY(0px);
+    -webkit-animation-timing-function: ease-out;
+    animation-timing-function: ease-out;
+    opacity: 1;
+  }
 }
 </style>
