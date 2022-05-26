9df961b8d374d8dd0fb158e2086e0c582dbb015c
Il Kwon <alexryu1105@naver.com>
2022-05-26 10:20:54 +0900

    아파트 조회수 로그인 된 유저들만 체크하지 않고, 모든 사용자로부터 체크하게 변경

--- src/main/java/com/ssafy/happy/deal/model/repo/DealAvgRepo.java
+++ src/main/java/com/ssafy/happy/deal/model/repo/DealAvgRepo.java
@@ -6,5 +6,5 @@ import java.util.List;
 
 public interface DealAvgRepo {
     List<DealAvg> getDealAvg(String aptCode);
-    void increaseHit(String aptCode);
+    void increaseHit(String aptName, String dongCode);
 }
