ad5a62b3d52e8f83cfcac8b05a61c26cc95ff7ee
choijoohee213 <choijoohee@naver.com>
2023-07-14 01:50:18 +0900

    refactor: mybatis 삭제 -> mapperScan 삭제

--- BackEnd/src/main/java/com/ssafy/happy/HappyApplication.java
+++ BackEnd/src/main/java/com/ssafy/happy/HappyApplication.java
@@ -1,12 +1,10 @@
 package com.ssafy.happy;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@MapperScan("com.ssafy.happy.**.model.repo")
 @PropertySource("classpath:/application.properties")
 public class HappyApplication {
 
