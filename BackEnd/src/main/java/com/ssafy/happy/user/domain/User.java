93b5d71df6f307f94ab38658c9bfd51d281d4e07
choijoohee213 <choijoohee@naver.com>
2023-07-13 18:16:40 +0900

    feat: 유저 엔티티 생성

---
+++ BackEnd/src/main/java/com/ssafy/happy/user/domain/User.java
@@ -0,0 +1,32 @@
+package com.ssafy.happy.user.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String nickname;
+
+    private String phone;
+
+    private String authority;
+}
