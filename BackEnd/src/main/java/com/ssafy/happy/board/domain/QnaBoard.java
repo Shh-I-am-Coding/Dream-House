fc40ac8e06f8df3baba88cd97368aed58de676b7
choijoohee213 <choijoohee@naver.com>
2023-07-20 23:26:43 +0900

    feat: 문의사항 게시판 엔티티 생성

---
+++ BackEnd/src/main/java/com/ssafy/happy/board/domain/QnaBoard.java
@@ -0,0 +1,29 @@
+package com.ssafy.happy.board.domain;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class QnaBoard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private int hit;
+
+    private LocalDateTime createdDate;
+}
