bbdc4ffc5d03c9d0e479c39a47f978f3075426cd
choijoohee213 <choijoohee@naver.com>
2023-07-21 02:41:20 +0900

    refactor: 문의사항 게시판 도메인 controller jpa로 리팩토링

---
+++ BackEnd/src/main/java/com/ssafy/happy/board/controller/QnaBoardController.java
@@ -0,0 +1,60 @@
+package com.ssafy.happy.board.controller;
+
+import com.ssafy.happy.board.dto.BoardSearchCondition;
+import com.ssafy.happy.board.dto.QnaBoardModifyRequest;
+import com.ssafy.happy.board.dto.QnaBoardRequest;
+import com.ssafy.happy.board.dto.QnaBoardResponse;
+import com.ssafy.happy.board.dto.QnaBoardSearchRequest;
+import com.ssafy.happy.board.service.QnaBoardService;
+import com.ssafy.happy.common.dto.ApiResponse;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/board")
+public class QnaBoardController {
+    private final QnaBoardService qnaBoardService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<?>> create(@RequestBody QnaBoardRequest qnaBoardRequest) {
+        qnaBoardService.create(qnaBoardRequest);
+        return ApiResponse.successWithNoContent();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<?>> update(@PathVariable Long id,
+                                                 @RequestBody QnaBoardModifyRequest qnaBoardModifyRequest) {
+        qnaBoardService.update(id, qnaBoardModifyRequest);
+        return ApiResponse.successWithNoContent();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<?>> delete(@PathVariable Long id) {
+        qnaBoardService.delete(id);
+		return ApiResponse.successWithNoContent();
+	}
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<QnaBoardResponse>> findBoard(@PathVariable Long id) {
+		return ApiResponse.successWithData(qnaBoardService.findOne(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<Page<QnaBoardResponse>>> search(@ModelAttribute QnaBoardSearchRequest qnaBoardSearchRequest) {
+        return ApiResponse.successWithData(qnaBoardService.findAll(qnaBoardSearchRequest));
+    }
+
+}
--- BackEnd/src/main/java/com/ssafy/happy/board/dto/QnaBoardSearchRequest.java
+++ BackEnd/src/main/java/com/ssafy/happy/board/dto/QnaBoardSearchRequest.java
@@ -1,13 +1,15 @@
 package com.ssafy.happy.board.dto;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class QnaBoardSearchRequest {
-    private String key;
+    private String key = "none";
     private String word;
     private int pageNum;
 }
