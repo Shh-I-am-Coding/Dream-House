4eb7a71ab07f500836d36674b902f1034b2ff243
choijoohee213 <choijoohee@naver.com>
2023-07-22 01:51:04 +0900

    feat: QnaBoard와 User 연관관계 설정 및 컬럼 제약조건 설정

--- BackEnd/src/main/java/com/ssafy/happy/board/domain/QnaBoard.java
+++ BackEnd/src/main/java/com/ssafy/happy/board/domain/QnaBoard.java
@@ -1,12 +1,16 @@
 package com.ssafy.happy.board.domain;
 
 import com.ssafy.happy.board.dto.QnaBoardModifyRequest;
+import com.ssafy.happy.user.domain.User;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,11 +29,14 @@ public class QnaBoard {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
+    @Column(length = 100, nullable = false)
     private String title;
 
-    @Column(columnDefinition = "TEXT")
+    @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
 
     @ColumnDefault("0")
