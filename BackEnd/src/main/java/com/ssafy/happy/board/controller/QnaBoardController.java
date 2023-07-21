18049f021db41bb37dc84bdee4638a2a72fdbdcc
choijoohee213 <choijoohee@naver.com>
2023-07-22 01:51:39 +0900

    feat: rest api uri 변경

--- BackEnd/src/main/java/com/ssafy/happy/board/controller/QnaBoardController.java
+++ BackEnd/src/main/java/com/ssafy/happy/board/controller/QnaBoardController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/board")
+@RequestMapping("/board/qna")
 public class QnaBoardController {
     private final QnaBoardService qnaBoardService;
 
