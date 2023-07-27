d5e6a70ba4a3b19a723fa2ac4c31d7753b3f3c9d
choijoohee213 <choijoohee@naver.com>
2023-07-28 01:04:20 +0900

    feat: swagger을 위한 board api 문서화

--- BackEnd/src/main/java/com/ssafy/happy/board/controller/BoardController.java
+++ BackEnd/src/main/java/com/ssafy/happy/board/controller/BoardController.java
@@ -6,6 +6,7 @@ import com.ssafy.happy.board.dto.BoardResponse;
 import com.ssafy.happy.board.dto.BoardSearchRequest;
 import com.ssafy.happy.board.service.BoardService;
 import com.ssafy.happy.common.dto.ApiResponse;
+import io.swagger.annotations.ApiOperation;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -27,12 +28,14 @@ public class BoardController {
     private final BoardService boardService;
 
     @PostMapping
+    @ApiOperation(value = "게시판 글 생성")
     public ResponseEntity<ApiResponse<?>> create(@RequestBody @Valid BoardRequest boardRequest) {
         boardService.create(boardRequest);
         return ApiResponse.successWithNoContent();
     }
 
     @PutMapping("/{id}")
+    @ApiOperation(value = "게시판 글 수정")
     public ResponseEntity<ApiResponse<?>> update(@PathVariable Long id,
                                                  @RequestBody @Valid BoardModifyRequest boardModifyRequest) {
         boardService.update(id, boardModifyRequest);
@@ -40,17 +43,20 @@ public class BoardController {
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "게시판 글 삭제")
     public ResponseEntity<ApiResponse<?>> delete(@PathVariable Long id) {
         boardService.delete(id);
         return ApiResponse.successWithNoContent();
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "게시판 글 상세 조회")
     public ResponseEntity<ApiResponse<BoardResponse>> findBoard(@PathVariable Long id) {
         return ApiResponse.successWithData(boardService.findOne(id));
     }
 
     @GetMapping
+    @ApiOperation(value = "게시판 글 목록 조회 - 페이징", notes = "글 검색도 제공")
     public ResponseEntity<ApiResponse<Page<BoardResponse>>> search(
             @ModelAttribute @Valid BoardSearchRequest boardSearchRequest) {
         return ApiResponse.successWithData(boardService.findAll(boardSearchRequest));
