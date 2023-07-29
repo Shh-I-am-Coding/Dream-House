fc26a68ad9a3088b7ec8b1bc711c861a5aa5cef8
Qulip <ilkwon.ryu@gmail.com>
2023-07-29 19:00:22 +0900

    feat: AptInfoResponse 생성

---
+++ BackEnd/src/main/java/com/ssafy/happy/deal/dto/AptInfoResponse.java
@@ -0,0 +1,18 @@
+package com.ssafy.happy.deal.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class AptInfoResponse {
+    private String name;
+    private Long average;
+    private String lat;
+    private String lng;
+}
