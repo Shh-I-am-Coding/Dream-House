f018b334cca4d2d7a8feaff12b35169230cbc127
choijoohee213 <choijoohee@naver.com>
2023-07-29 18:01:50 +0900

    refactor: api uri 복수형으로 변경

--- BackEnd/src/main/java/com/ssafy/happy/board/controller/BoardController.java
+++ BackEnd/src/main/java/com/ssafy/happy/board/controller/BoardController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/board")
+@RequestMapping("/boards")
 public class BoardController {
     private final BoardService boardService;
 
