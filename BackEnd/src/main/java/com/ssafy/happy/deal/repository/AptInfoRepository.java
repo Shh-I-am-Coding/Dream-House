260ecd9bdb8b5a90e0ff7aeae43003f6e6278034
Qulip <ilkwon.ryu@gmail.com>
2023-08-02 00:20:27 +0900

    feat: AptInfoRepository 동검색 JPQL 수정

--- BackEnd/src/main/java/com/ssafy/happy/deal/repository/AptInfoRepository.java
+++ BackEnd/src/main/java/com/ssafy/happy/deal/repository/AptInfoRepository.java
@@ -1,15 +1,21 @@
 package com.ssafy.happy.deal.repository;
 
-import com.ssafy.happy.deal.domain.AptInfo;
 import java.util.List;
+
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import com.ssafy.happy.deal.domain.AptInfo;
+import com.ssafy.happy.deal.dto.AptInfoResponse;
+
 public interface AptInfoRepository extends JpaRepository<AptInfo, Long> {
 
     @EntityGraph(attributePaths = {"deal"}, type = EntityGraphType.FETCH)
-    @Query("select a from AptInfo a left join a.deals where a.emdArea.code = :code")
-    List<AptInfo> getAptInfoByEmdArea(String code);
+    @Query("select new com.ssafy.happy.deal.dto.AptInfoResponse(a.name, avg(d.dealAmount), a.lat, a.lng) "
+        + "from AptInfo a left join a.deals d "
+        + "where a.emdArea.code = :code "
+        + "group by a.id")
+    List<AptInfoResponse> getAptInfoByEmdArea(String code);
 }
