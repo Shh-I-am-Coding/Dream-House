4062e332921605767e8b381044bf235bfa62737e
Qulip <ilkwon.ryu@gmail.com>
2023-08-16 01:48:27 +0900

    fix: AptInfoRepository EntityGraph 어노테이션 및 반환타입 수정

--- BackEnd/src/main/java/com/ssafy/happy/deal/repository/AptInfoRepository.java
+++ BackEnd/src/main/java/com/ssafy/happy/deal/repository/AptInfoRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.happy.deal.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
@@ -12,16 +13,18 @@ import com.ssafy.happy.deal.dto.AptInfoResponse;
 
 public interface AptInfoRepository extends JpaRepository<AptInfo, Long> {
 
-    @EntityGraph(attributePaths = {"deal"}, type = EntityGraphType.FETCH)
+    // todo: EntityGraph 자세히 알아보기
+
+    //  @EntityGraph(attributePaths = {"dealList"}, type = EntityGraphType.FETCH)
     @Query("select new com.ssafy.happy.deal.dto.AptInfoResponse(a.name, avg(d.dealAmount), a.lat, a.lng) "
         + "from AptInfo a left join a.dealList d "
         + "where a.emdArea.code = :code "
         + "group by a.id")
     List<AptInfoResponse> getAptInfoByEmdArea(String code);
 
-    @EntityGraph(attributePaths = {"deal, roadName, emdArea"}, type = EntityGraphType.FETCH)
+    @EntityGraph(attributePaths = {"dealList, roadName, emdArea"}, type = EntityGraphType.FETCH)
     @Query("select a "
         + "from AptInfo a left join a.dealList d "
         + "where a.id = :id ")
-    AptInfo getAptInfoById(Long id);
+    Optional<AptInfo> getAptInfoById(Long id);
 }
