3c0a4099ab8c535e398106aca6e75d21ac991660
Qulip <ilkwon.ryu@gmail.com>
2023-08-18 17:35:27 +0900

    feat: DealService 아파트 상세정보 조회 추가

--- BackEnd/src/main/java/com/ssafy/happy/deal/service/DealService.java
+++ BackEnd/src/main/java/com/ssafy/happy/deal/service/DealService.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ssafy.happy.deal.domain.AptInfo;
+import com.ssafy.happy.deal.dto.AptInfoDetailResponse;
 import com.ssafy.happy.deal.dto.AptInfoResponse;
+import com.ssafy.happy.deal.exception.NotExistAptInfoException;
 import com.ssafy.happy.deal.repository.AptInfoRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -22,5 +25,12 @@ public class DealService {
 		List<AptInfoResponse> aptInfoResponseList = aptInfoRepository.getAptInfoByEmdArea(code);
 		// todo : Redis 캐싱 처리 추가
 		return aptInfoResponseList;
+	}
+
+	@Transactional(readOnly = true)
+	public AptInfoDetailResponse getAptInfoDetail(Long id) {
+		AptInfo aptInfo = aptInfoRepository.getAptInfoById(id)
+			.orElseThrow(NotExistAptInfoException::new);
+		return AptInfoDetailResponse.of(aptInfo);
 	}
 }
