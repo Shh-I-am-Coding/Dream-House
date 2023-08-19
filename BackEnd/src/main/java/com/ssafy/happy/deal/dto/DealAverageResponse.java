a322163960c85057b1c9e7204052e0cccd0b23b7
Qulip <ilkwon.ryu@gmail.com>
2023-08-20 02:58:13 +0900

    feat: DealAverageResponse 생성

---
+++ BackEnd/src/main/java/com/ssafy/happy/deal/dto/DealAverageResponse.java
@@ -0,0 +1,17 @@
+package com.ssafy.happy.deal.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class DealAverageResponse {
+	private int dealYear;
+	private long dealAverage;
+	private long pricePerArea;
+}
