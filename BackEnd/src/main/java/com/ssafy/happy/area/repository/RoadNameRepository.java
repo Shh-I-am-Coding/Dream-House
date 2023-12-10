bbbf66f074a687640a7b73789a590d08e5a7e8de
Qulip <ilkwon.ryu@gmail.com>
2023-12-10 20:43:44 +0900

    feat: RoadNameRepository 생성

---
+++ BackEnd/src/main/java/com/ssafy/happy/area/repository/RoadNameRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.happy.area.repository;
+
+import com.ssafy.happy.area.domain.RoadName;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoadNameRepository extends JpaRepository<RoadName, Long> {
+
+}
