04d7fdbd1d1382faf9b87fa3bcdc54b05169ac8f
Qulip <ilkwon.ryu@gmail.com>
2023-12-10 22:24:22 +0900

    fix: SidoAreaRepository Optional 추가

--- BackEnd/src/main/java/com/ssafy/happy/area/repository/SidoAreaRepository.java
+++ BackEnd/src/main/java/com/ssafy/happy/area/repository/SidoAreaRepository.java
@@ -2,10 +2,11 @@ package com.ssafy.happy.area.repository;
 
 import com.ssafy.happy.area.domain.SidoArea;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SidoAreaRepository extends JpaRepository<SidoArea, Long> {
     List<SidoArea> findAll();
 
-    SidoArea findSidoAreaById(Long code);
+    Optional<SidoArea> findSidoAreaById(Long code);
 }
