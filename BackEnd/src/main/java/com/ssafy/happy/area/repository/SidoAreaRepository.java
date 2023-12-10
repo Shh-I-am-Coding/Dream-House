c274111a387e331f6e59d0257e4c07fe506f2f72
Qulip <ilkwon.ryu@gmail.com>
2023-12-10 20:45:58 +0900

    feat: SidoAreaRepository 생성

---
+++ BackEnd/src/main/java/com/ssafy/happy/area/repository/SidoAreaRepository.java
@@ -0,0 +1,9 @@
+package com.ssafy.happy.area.repository;
+
+import com.ssafy.happy.area.domain.SidoArea;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SidoAreaRepository extends JpaRepository<SidoArea, Long> {
+    List<SidoArea> findSidoAreasByName(String name);
+}
