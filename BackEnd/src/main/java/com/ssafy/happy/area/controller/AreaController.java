c2379b396d7ba87ac3375ccf4a23d67e9c363649
Qulip <ilkwon.ryu@gmail.com>
2023-12-10 22:16:35 +0900

    feat: AreaController 전체 시도 조회 추가

--- BackEnd/src/main/java/com/ssafy/happy/area/controller/AreaController.java
+++ BackEnd/src/main/java/com/ssafy/happy/area/controller/AreaController.java
@@ -1,8 +1,12 @@
 package com.ssafy.happy.area.controller;
 
+import com.ssafy.happy.area.dto.SidoAreaResponse;
 import com.ssafy.happy.area.service.AreaService;
+import io.swagger.annotations.ApiOperation;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +18,9 @@ public class AreaController {
 
     private final AreaService areaService;
 
+    @GetMapping("/sido")
+    @ApiOperation(value = "전체 시도 조회", notes = "대한민국 전체 시도 이름, 코드 조회")
+    public List<SidoAreaResponse> getAllSidoArea() {
+        return areaService.getAllSidoArea();
+    }
 }
