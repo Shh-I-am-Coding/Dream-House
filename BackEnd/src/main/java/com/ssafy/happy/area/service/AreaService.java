db4987a8378c642cb055f9e605d3b45b8c58f15b
Qulip <ilkwon.ryu@gmail.com>
2023-12-10 22:28:40 +0900

    feat: AreaService 시군구 검색 추가

--- BackEnd/src/main/java/com/ssafy/happy/area/service/AreaService.java
+++ BackEnd/src/main/java/com/ssafy/happy/area/service/AreaService.java
@@ -1,8 +1,12 @@
 package com.ssafy.happy.area.service;
 
+import com.ssafy.happy.area.domain.SidoArea;
 import com.ssafy.happy.area.dto.SidoAreaResponse;
+import com.ssafy.happy.area.dto.SiggAreaResponse;
+import com.ssafy.happy.area.exception.NotExistSidoAreaException;
 import com.ssafy.happy.area.repository.EmdAreaRepository;
 import com.ssafy.happy.area.repository.SidoAreaRepository;
+import com.ssafy.happy.area.repository.SiggAreaRepository;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -15,9 +19,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class AreaService {
 
     private final SidoAreaRepository sidoAreaRepository;
+    private final SiggAreaRepository siggAreaRepository;
     private final EmdAreaRepository emdAreaRepository;
 
     public List<SidoAreaResponse> getAllSidoArea() {
         return sidoAreaRepository.findAll().stream().map(SidoAreaResponse::of).collect(Collectors.toList());
+    }
+
+    public List<SiggAreaResponse> getSiggArea(Long code) {
+        SidoArea sidoArea = sidoAreaRepository.findSidoAreaById(code)
+                .orElseThrow(NotExistSidoAreaException::new);
+        return siggAreaRepository.findAllBySidoArea(sidoArea).stream().map(SiggAreaResponse::of)
+                .collect(Collectors.toList());
     }
 }
