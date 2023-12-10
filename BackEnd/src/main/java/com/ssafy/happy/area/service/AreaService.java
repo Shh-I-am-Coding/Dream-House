972a7db77266d8da956b7386c44136077dcd35e1
Qulip <ilkwon.ryu@gmail.com>
2023-12-10 22:09:03 +0900

    feat: AreaService 시도 전체 검색 추가

--- BackEnd/src/main/java/com/ssafy/happy/area/service/AreaService.java
+++ BackEnd/src/main/java/com/ssafy/happy/area/service/AreaService.java
@@ -1,16 +1,23 @@
 package com.ssafy.happy.area.service;
 
+import com.ssafy.happy.area.dto.SidoAreaResponse;
+import com.ssafy.happy.area.repository.EmdAreaRepository;
+import com.ssafy.happy.area.repository.SidoAreaRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.ssafy.happy.area.repository.EmdAreaRepository;
-
-import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class AreaService {
 
-    private final EmdAreaRepository areaRepository;
+    private final SidoAreaRepository sidoAreaRepository;
+    private final EmdAreaRepository emdAreaRepository;
+
+    public List<SidoAreaResponse> getAllSidoArea() {
+        return sidoAreaRepository.findAll().stream().map(SidoAreaResponse::of).collect(Collectors.toList());
+    }
 }
